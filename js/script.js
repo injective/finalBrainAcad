df7ba3ce89ee98ee848a38d57b1509fb78bcb438
Vadim Sereda <flash.brovary@gmail.com>
2016-04-13 19:07:45 +0300

    change portfolio.html

---
+++ js/script.js
@@ -0,0 +1,9 @@
+var thumb = document.querySelectorAll('.thumbs-item');
+for (var i = 0; i < thumb.length; i++) {
+    thumb.addEventListener('mouseover', function(){
+        var thumbImg = document.querySelectorAll('.thumbs-item-img');
+        var thumbTitle = document.querySelectorAll('.thumbs-item-title');
+
+
+    }, false);
+}
