25d61c6eefecceece34c8ee2c6f2cab590ab251b
Vadim Sereda <flash.brovary@gmail.com>
2016-04-17 20:01:02 +0300

    finished responce

--- js/script.js
+++ js/script.js
@@ -1,8 +1,22 @@
-var thumb = document.querySelectorAll('.thumbs-item');
-for (var i = 0; i < thumb.length; i++) {
-    thumb.addEventListener('mouseover', function() {
-        var thumbImg = document.querySelectorAll('.thumbs-item-img');
-        var thumbTitle = document.querySelectorAll('.thumbs-item-title');
+$(function(){
+    $('.thumbs-item').mouseover(function(){
+        $(this).find('.thumbs-item-title').css({
+            color: '#444'
+        });
+        $(this).find('.thumbs-item-cover').css({
+            background: 'linear-gradient(rgba(153,129,64,0.8),rgba(153,129,64,0.8)),url(img/thumb.jpg) no-repeat'
+        });
+    });
 
-    }, false);
-}
+    $('.thumbs-item').mouseout(function(){
+        $(this).find('.thumbs-item-title').css({
+            color: '#9B8440'
+        });
+
+        $(this).find('.thumbs-item-cover').css({
+            background: 'url(img/thumb.jpg) no-repeat'
+        });
+    });
+
+
+});
